9143399a32dd4ca6c718d8ebdbcef9ddba5d91c6
zhangzeyu <zzy_workspace@163.com>
2020-04-20 13:59:31 +0800

    继续完善脚本

--- about-concurrent/bin/windows/threaddump.bat
+++ about-concurrent/bin/windows/threaddump.bat
@@ -1,14 +1,12 @@
 @echo off
-setlocal enabledelayedexpansion
-
 
 set processName=%1
 
 rem processName
+:waitInput
 if "%processName%"=="" (
-	echo Error: Not get target process name
-	echo Suggestion: Please input process name as first param
-	exit /b 1
+	set /p processName=Please input Java process Name:
+	goto waitInput
 )
 
 rem JAVA_HOME
@@ -22,7 +20,31 @@ set jps=%java_home%\bin\jps.exe
 if not exist "%jps%" (
 	echo Error: Not found %java_home%\bin\jps.exe
 	echo Suggestion: Please check environment variable JAVA_HOME available
+	pause
 	exit /b 1
+)
+
+set pid=
+setlocal enabledelayedexpansion
+for /f "usebackq tokens=1,2*" %%i in (`"%jps%" -l`) do (
+	rem %%i java pid
+	rem %%j java process name
+
+	rem search process name
+	set pname=%%j
+	@echo !pname!|findstr "%processName%">nul
+	if !errorlevel!==0 (
+		set pid=%%i
+	)
+)
+
+
+if "!pid!"=="" (
+	echo Not Found pid by %processName%
+	pause
+	exit /b 1
+) else (
+	echo Found pid !pid!
 )
 
 rem jstack
@@ -30,29 +52,19 @@ set jstack=%java_home%\bin\jstack.exe
 if not exist "%jstack%" (
 	echo Error: Not found %java_home%\bin\jstack.exe
 	echo Suggestion: Please check environment variable JAVA_HOME available
+	pause
 	exit /b 1
 )
 
-set pid=
-for /f "usebackq tokens=1,2*" %%i in (`"%jps%" -l`) do (
-	rem %%i java pid
-	rem %%j java process name
-	
-	
-	rem search process name
-	set pname=%%j
-	@echo !pname!|findstr "%processName%">nul
-	if !errorlevel!==0 (
-		set pid=%%i
-		echo Found pid !pid!
-	)
-	
+if not exist D:\logs\. (
+	mkdir D:\logs
+)
+rem generate tdump file. echo off
+cmd /q /c  "%jstack%" -l !pid!>D:\logs\!pid!.tdump
+if !errorlevel!==0 (
+	echo output file: D:\logs\!pid!.tdump
 )
 
-if "%pid%"=="" (
-	echo Not Found pid by %processName%
-	exit /b 1
-)
-
+endlocal
 
 pause
