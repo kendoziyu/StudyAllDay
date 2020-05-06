e0d4484afbe8ed819c7364ded69e1726b7d74fe0
zhangzeyu <zzy_workspace@163.com>
2020-05-06 19:11:03 +0800

    新增消费者生产者例子

---
+++ about-concurrent/src/main/java/condition/BoundedBuffer.java
@@ -0,0 +1,90 @@
+package condition;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BoundedBuffer {
+
+    final Lock lock = new ReentrantLock();
+    final Condition notFull  = lock.newCondition();
+    final Condition notEmpty = lock.newCondition();
+
+    final Object[] items = new Object[100];
+    int putptr, takeptr, count;
+
+    public void put(Object x) throws InterruptedException {
+        lock.lock();
+        try {
+            // 判断
+            while (count == items.length) {
+                notFull.await();
+            }
+            // 执行
+            items[putptr] = x;
+            if (++putptr == items.length) {
+                putptr = 0;
+            }
+            ++count;
+            // 通知
+            notEmpty.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Object take() throws InterruptedException {
+        lock.lock();
+        try {
+            // 判断
+            while (count == 0) {
+                notEmpty.await();
+            }
+            // 执行
+            Object x = items[takeptr];
+            if (++takeptr == items.length) {
+                takeptr = 0;
+            }
+            --count;
+            // 通知
+            notFull.signal();
+            return x;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        final BoundedBuffer buffer = new BoundedBuffer();
+        Runnable consumer = new Runnable() {
+            public void run() {
+                try {
+                    for (int i = 0; i < 101; i++) {
+                        Object item = buffer.take();
+                        System.out.println(item);
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Runnable producer = new Runnable() {
+            public void run() {
+                try {
+                    for (int i = 1; i <= 101; i++) {
+                        buffer.put(i);
+                        Thread.sleep(10000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        new Thread(consumer, "consumer").start();
+        new Thread(producer, "producer").start();
+
+    }
+}
