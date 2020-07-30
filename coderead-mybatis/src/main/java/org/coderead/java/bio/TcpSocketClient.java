2be478c30ff01544a4cafafdb2c905f1a6811b57
zzy_workspace <zzy_workspace@163.com>
2020-07-30 23:45:46 +0800

    Tcp Socket 套接字

---
+++ coderead-mybatis/src/main/java/org/coderead/java/bio/TcpSocketClient.java
@@ -0,0 +1,54 @@
+package org.coderead.java.bio;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class TcpSocketClient {
+
+    public static void main(String[] args) throws IOException {
+        // 创建一个还未被连接的套接字
+        Socket socket = new Socket();
+        System.out.println("客户机的套接字是否已被连接？" + socket.isConnected());
+        // 为本地主机创建一个 InetAddress 对象
+        InetAddress localHost = InetAddress.getLocalHost();
+        // 主动连接服务器
+        socket.connect(new InetSocketAddress(localHost, 8080));
+        System.out.println("客户机的套接字是否已被连接？" + socket.isConnected());
+        OutputStream outStream = socket.getOutputStream();
+        InputStream inStream = socket.getInputStream();
+        // 用 PrintWriter 装饰输出流
+        PrintWriter out = new PrintWriter(outStream);
+        // 用 Scanner 装饰输入流
+        Scanner in = new Scanner(inStream);
+        Scanner console = new Scanner(System.in);
+        long sendTime;
+        long returnTime;
+        // 阻塞，等待控制台输入数据
+        while (console.hasNextLine()) {
+            // 将输入台数据传输给服务器
+            sendTime = System.currentTimeMillis();
+            out.write(console.nextLine());
+            // 阻塞，等待读取服务器回写数据
+            while (in.hasNextLine()) {
+                returnTime = System.currentTimeMillis();
+                System.out.println("共计等待" + (returnTime-sendTime) +" ms 获取服务器数据");
+                // 将服务器回传数据输出到控制台
+                System.out.println(in.nextLine());
+            }
+        }
+        console.close();
+        System.out.println("客户端套接字是否已经关闭？" + socket.isClosed());
+        in.close();
+        System.out.println("客户端套接字是否已经关闭？" + socket.isClosed());
+        out.close();
+    }
+}
---
+++ coderead-mybatis/src/main/java/org/coderead/java/bio/TcpSocketServer.java
@@ -0,0 +1,39 @@
+package org.coderead.java.bio;
+
+import org.apache.log4j.net.SocketServer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class TcpSocketServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket serverSocket = new ServerSocket();
+        // 绑定端口
+        serverSocket.bind(new InetSocketAddress(8080));
+        System.out.println("服务器等待连接...  127.0.0.1:8080");
+        // 阻塞，等待客户机连接
+        Socket clientSocket = serverSocket.accept();
+        InetSocketAddress clientAddress = (InetSocketAddress) clientSocket.getRemoteSocketAddress();
+        System.out.println("接收到客户端连接" + clientAddress.getHostString() + ":" + clientAddress.getPort());
+
+        OutputStream out = clientSocket.getOutputStream();
+        InputStream in = clientSocket.getInputStream();
+        byte[] readBytes = new byte[1024];
+        // 阻塞，等待读取客户机数据
+        while (in.read(readBytes) != -1) {
+            System.out.println(new String(readBytes));
+            // 阻塞，代表服务器业务处理
+            Thread.sleep(200);
+            // 服务器向客户端回写数据
+            out.write(String.valueOf(System.currentTimeMillis()).getBytes());
+        }
+        clientSocket.close();
+        serverSocket.close();
+    }
+}
