6c928bd436b4b0b0d2cfe8e466a6b4fd852a9faf
zzy_workspace <zzy_workspace@163.com>
2020-08-04 08:07:35 +0800

    并发连接测试

---
+++ coderead-mybatis/src/main/java/org/coderead/java/bio/SyncClients.java
@@ -0,0 +1,20 @@
+package org.coderead.java.bio;
+
+import java.net.Socket;
+
+/**
+ * 描述:  <br>
+ *
+ * @author: kendo ziyu <br>
+ * @date: 2020/8/4 0004 <br>
+ */
+public class SyncClients {
+
+    private static Socket[] clients = new Socket[5];
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= clients.length; i++) {
+            clients[i-1] = new Socket("127.0.0.1", 8080);
+            System.out.println("client connection:" + i);
+        }
+    }
+}
